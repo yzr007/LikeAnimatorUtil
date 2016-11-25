d1d04aedd32d660eeb340d16e2a8f37e15044714
yzr007 <yangzr1990@gmail.com>
2016-11-25 23:13:58 +0800

    为方法调用增加注释

--- demo/src/main/java/com/yzr/likeanimatorutildemo/MainActivity.java
+++ demo/src/main/java/com/yzr/likeanimatorutildemo/MainActivity.java
@@ -32,6 +32,23 @@ public class MainActivity extends AppCompatActivity{
     private void showAnim(){
         int[] imgs = {R.mipmap.heart_01,R.mipmap.heart_02,R.mipmap.heart_03,R.mipmap.heart_04,R.mipmap.heart_05};
         likeAnimatorManager = new LikeAnimatorManager(this,txt,imgs);
+        //动画视图的大小和位置默认为target的大小和位置，可以通过setLocation()和setSize()自由配置
+        likeAnimatorManager.setAnimDelay(50);//单个动画的间隔，默认200ms
+        likeAnimatorManager.setAnimCount(50);//点赞动画的数量，默认20
+        likeAnimatorManager.setAnimDuration(1000);//单个动画的Duration，默认2000ms
+        likeAnimatorManager.setAnimPathHeight(1000);//动画轨迹的高度，默认600
+        likeAnimatorManager.setAnimHorizontalScope(600);//动画轨迹的横向最大范围，默认200
+        likeAnimatorManager.setAlphaAble(true);//是否开启透明度变化，默认false
+        likeAnimatorManager.setStartAlpha(0.2f);//起始透明度
+        likeAnimatorManager.setEndAlpha(1f);//结束透明度
+        likeAnimatorManager.setScaleAble(true);//是否开启缩放，默认false
+        likeAnimatorManager.setScaleScope(1.2f);//缩放动画的缩放范围：1-（1+scope），默认0.5,最小值 -1
         likeAnimatorManager.play();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        likeAnimatorManager.cancel();
     }
 }
